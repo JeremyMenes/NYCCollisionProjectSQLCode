6061f552dc1c43b31a4616d5f3be353a93ec4e71
JeremyMenes <menesjeremy@gmail.com>
2025-03-23 23:14:41 -0700

    Update Update Single Column From Another Table.sql

--- Update Single Column From Another Table.sql
+++ Update Single Column From Another Table.sql
@@ -1,3 +1,5 @@
+--Restores original data in a specific column from the backup database 
+
 UPDATE Target
 SET Target.[VEHICLE_TYPE_CODE_4] = OG.[VEHICLE_TYPE_CODE_4]
 FROM [NYC_Collisions].[dbo].[Motor_Vehicle_Collisions_-_Crashes] Target
